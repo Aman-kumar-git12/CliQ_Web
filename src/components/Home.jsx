48056fd3206467fda9f0850a7680333b08ad9a4a
Aman kumar <rajaman5562@gmail.com>
2025-11-27 23:37:04 +0530

    fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import PostCard from "./Postcard";
+// import PostCard from "./Postcard";
 export default function Home() {
     return (
       <h1 className="text-2xl font-bold text-center text-black dark:text-white  p-4 border-b border-neutral-200 dark:border-neutral-800">This is my feed page </h1>
